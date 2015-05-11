621989253e5322464c7b8a9f6f80e5f170ce5f7a
Yagiz Nizipli <yagiznizipli@me.com>
2015-05-12 02:01:36 +0300

    Add initial view

--- assets/js/controllers/MainController.js
+++ assets/js/controllers/MainController.js
@@ -1,5 +1,27 @@
 'use strict';
 
-angular.module('aloApp').controller('MainController', function($scope) {
+angular.module('aloApp').controller('MainController', function($scope, $http) {
     $scope.activeView = 'template';
+
+    $scope.getTemplates = function() {
+        $http.get('http://signa.dev/assets/data.json', {
+            request: -1,
+            type: 1,
+            page: 0
+        })
+            .success(function(response) {
+                $scope.templates = response;
+                console.log(response);
+            });
+    };
+
+    $scope.getTemplate = function(id) {
+        $http.get('http://signa.dev/assets/template.xml')
+            .success(function(response) {
+                $scope.template = response;
+                console.log(response);
+            });
+    };
+
+    $scope.getTemplates();
 });
