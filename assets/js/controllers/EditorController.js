e0026aa94f62ba49371fff9002c4564f3d4bd4fa
Yagiz Nizipli <yagiznizipli@me.com>
2015-05-13 03:14:04 +0300

    Add texts to canvas

--- assets/js/controllers/EditorController.js
+++ assets/js/controllers/EditorController.js
@@ -59,6 +59,29 @@ angular.module('aloApp').controller('EditorController', function($scope, $rootSc
             width: $scope.fabric.canvasOriginalWidth,
             height: $scope.fabric.canvasOriginalHeight
         });
+
+        $scope.fabric.getCanvas().clear();
+
+        data['design']['page']['layout']['group'].forEach(function(text) {
+            var scaleFactor = 1;
+
+            var addedText = new fabric.IText(decodeURIComponent(text['text']['_value'], {
+                left: text['_leftx'] * scaleFactor,
+                top: text['_topy'] * scaleFactor,
+                fontFamily: text.text.font._fontface,
+                fontSize: text.text.font._fontsize,
+                fontWeight: (text.text.font._fontbold == "1" ? "bold" : "normal"),
+                fontStyle: (text.text.font._fontitalic == "1" ? "italic" : "normal"),
+                textDecoration: (text.text.font._fontul == "1" ? "underline" : "none"),
+                textAlign: text.text.font._fontalign,
+                cursorColor: text.text.font._fontcolor,
+                fill: text.text.font._fontcolor
+            })).scale(scaleFactor);
+
+            $scope.fabric.getCanvas().add(addedText);
+        });
+
+        $scope.fabric.getCanvas().renderAll();
     });
 
     Keypress.onSave(function() {
