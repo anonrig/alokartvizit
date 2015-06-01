9cee002be7d08a3b0c5a5b26deca70ba6f7c333f
Yagiz Nizipli <yagiznizipli@me.com>
2015-06-02 02:35:39 +0300

    Remove unnecessary files in fonts directory

--- assets/fonts/helper.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$files = scandir(".", 1);
-        for ($i = 0; $i < count($files); $i++) {
-            if(is_dir($files[$i]) && $files[$i] != "." && $files[$i] != "..")
-            echo $files[$i] . "<br>"; 
-        }
